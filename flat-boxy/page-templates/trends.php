4def63e24e3dcc6bbf41a6bfca2fa591b5aa6096
windsolace <digital_trainer_269@hotmail.com>
2015-04-30 23:18:11 +0800

    Committed missing files for hydiTrends

--- flat-boxy/page-templates/trends.php
+++ flat-boxy/page-templates/trends.php
@@ -47,25 +47,24 @@ get_header(); ?>
 						<input type="submit" id="searchsubmit" value="Search" />
 					</div>
 				</form></p>
-				
-				<div class = "cell trend-cell">
-					<h3 class = "">Top Searches
-						<span class = "subtitle">Australia</span>
-					</h3>
-					<ul class = "no-list">
-						<li>trend 1</li>
-						<li>trend 2</li>
-						<li>trend 3</li>
-						<li>trend 4</li>
-						<li>trend 5</li>
-						<li>trend 6</li>
-						<li>trend 7</li>
-						<li>trend 8</li>
-						<li>trend 9</li>
-						<li>trend 10</li>
-					</ul>
-
+			
+				<div id = "top-searches" class = "cell trend-cell">
+					<script id = "top-searches-template" type = "text/html">
+						<h3 class = "">Top Searches
+							<span class = "subtitle"><%= data.list.topSearches.country.name %></span>
+						</h3>
+						<ul class = "no-list">
+							<% var count = 0; %>
+							<% _.each(data.list.topSearches.trendList, function(item){ %>
+								<% if(count < 10) { %>
+									<li><a href = "http://www.google.com/#q=<%= item.title %>&tbm=nws" target="_blank"><%= item.title %></a></li>
+								<% count++} %>
+							<% }); %>
+						</ul>
+					</script>
 				</div>
+				
+
 				<div class = "cell trend-cell">
 					<h3 class = "">Eat
 						<span class = "subtitle">Singapore</span>
@@ -99,12 +98,18 @@ get_header(); ?>
 						<li>trend 9</li>
 						<li>trend 10</li>
 					</ul>
-				</div>
+				</div>	
 			</div> <!-- .cell-container -->
 
 		</div><!-- #main-content -->		
 	</section><!-- #main -->
 
 	<?php get_footer();?>	
+<script>
+	$(document).ready(function(){
+		var trendsResponse = <?php echo hydi_getTrends("SG") ?>;
+		trendsFn(trendsResponse);
+	});
+</script>
 
-?>
+
