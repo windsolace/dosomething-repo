ba85fe18bf8d8040cda2b17f25cd291aa95989ef
windsolace <eugene.nwm@gmail.com>
2015-05-25 23:08:10 +0800

    Merged PROD and trunk

--- flat-boxy/js/cells.js
+++ flat-boxy/js/cells.js
@@ -1,3 +1,36 @@
+//javascript file
+$(document).ready(function(){
+    //$("body").css("display","none");
+    //$("body").fadeIn(1000);
+    $('#searchform').find('label.screen-reader-text').text('');
+    /*
+    *Effects when cell is clicked
+    */
+    $('div.homecell').click(function(){ 
+        var clickedCell = $(this);
+        var homecellArr = $('.homecell');
+        for(var i = 0; i < homecellArr.length; i++){
+            var currentLoopEle = homecellArr[i];
+            if(currentLoopEle.className != clickedCell[0].className){
+                //$(currentLoopEle).css('-webkit-animation', 'cellclick 1s');
+                $(currentLoopEle).fadeTo('slow', 0);
+            }
+        }
+        fadeScreen(500);    
+    });
+
+    $('.cell-content span, #breadcrumbs span').click(function(){
+        //fadeScreen(0);
+    });
+
+    function fadeScreen(timing){
+        if(timing >= 0){
+            $("html").delay(timing).fadeOut();  
+        }
+    }
+
+})
+
 window.onload = function() {
     $('span#menu-icon').click(function() {
         if ($('#navigation.open').length) {
