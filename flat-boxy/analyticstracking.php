f95ca14fd3691b2b027246e5adcf5613a9e17050
windsolace <eugene.nwm@gmail.com>
2015-05-27 02:21:50 +0800

    Added link tracking for Google Analytics

--- flat-boxy/analyticstracking.php
+++ flat-boxy/analyticstracking.php
@@ -5,6 +5,7 @@
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-56630042-1', 'auto');
+  ga('require', 'linkid', 'linkid.js');
   ga('send', 'pageview');
 
 </script>
