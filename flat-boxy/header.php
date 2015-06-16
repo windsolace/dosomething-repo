ec0b6e8d81fde9e1a6e4ad2efa8f49a0ad4a29be
windsolace <digital_trainer_269@hotmail.com>
2015-06-16 16:59:00 +0800

    Analytics: Updated Google tag manager code to correct code

--- flat-boxy/header.php
+++ flat-boxy/header.php
@@ -100,13 +100,13 @@
 	?>
 
 	<!-- Google Tag Manager -->
-	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KW857C"
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KBQK3H"
 	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-KW857C');</script>
+	})(window,document,'script','dataLayer','GTM-KBQK3H');</script>
 	<!-- End Google Tag Manager -->
 
 	<script>
