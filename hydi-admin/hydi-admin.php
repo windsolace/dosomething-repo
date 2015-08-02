b183d7f12a71f329d6b43007076a469850ca288b
windsolace <eugene.nwm@gmail.com>
2015-08-02 23:07:53 +0800

    Initial commit of hydi-admin plugin files. Added HTML form for adding activity. Added necessary menu items into wp-admin page.

---
+++ hydi-admin/hydi-admin.php
@@ -0,0 +1,114 @@
+<?php 
+/**
+ *@package Hydi_Admin
+ */
+    /**
+    * Plugin Name: Hydi Admin
+    * Plugin URI: http://hydi.voqux.com
+    * Description: For administration of HYDI and its database
+    * Author: Eugene Ng
+    * Version: 1.0
+    * Author URI: http://hydi.voqux.com
+    **/
+
+    /*==============================================/
+    Contents
+    HA-00. Main
+    HA-01. init()
+        HA-01a. hydi_admin_theme_style()
+        HA-01b. adminMenu_init()
+    ===============================================*/
+
+    /**
+    * HA-00. Main
+    */
+    hydi_admin_init();
+
+    /**
+    * HA-01. init
+    *   a. Call CSS
+    *   b. Initialize admin menu
+    */
+    function hydi_admin_init(){
+        //HA-01a. Call CSS
+        hydi_admin_theme_style();
+
+        //HA-01b. Initialize admin menu
+        adminMenu_init();
+
+    }
+
+    /**
+    * HA-01a. Enqueue custom CSS
+    */
+    function hydi_admin_theme_style() {
+	    wp_enqueue_style('hydi-admin-theme', plugins_url('wp-admin.css', __FILE__));
+
+        wp_enqueue_script('jquery');
+        wp_enqueue_script('jquery-timepicker',  
+            '//cdnjs.cloudflare.com/ajax/libs/jquery-timepicker/1.8.1/jquery.timepicker.min.js',
+            array('jquery'));            
+	}
+
+    /**
+    * HA-01b. adminMenu_init
+    * Add menu items into the wordpress admin menu
+    * - HYDI
+    */
+    function adminMenu_init(){
+        //HA-01b. Hook for adding admin menus
+        add_action('admin_menu', 'mt_add_pages');
+
+        // action function for above hook
+        function mt_add_pages() {
+            // Add a new top-level menu:
+            add_menu_page(__('Hydi Admin','menu-test'), __('HYDI','menu-test'), 'manage_options', 'hydi-admin', null );
+
+            // Add a submenu to the custom top-level menu:
+            add_submenu_page('hydi-admin', __('Add an Activity','menu-test'), __('Add Activity','menu-test'), 'manage_options', 'hydi-admin', 'hydi_add_activity_page');
+
+            // Add a second submenu to the custom top-level menu:
+            add_submenu_page('hydi-admin', __('Update an Activity','menu-test'), __('Update Activity','menu-test'), 'manage_options', 'update', 'hydi_update_activity_page');
+            add_submenu_page('hydi-admin', __('Pending Requests','menu-test'), __('Pending Activities','menu-test'), 'manage_options', 'pending', 'hydi_pending_activities_page');
+
+            // Add a new submenu under Settings:
+            //add_options_page(__('Test Settings','menu-test'), __('Test Settings','menu-test'), 'manage_options', 'testsettings', 'mt_settings_page');
+
+            // Add a new submenu under Tools:
+            //add_management_page( __('Test Tools','menu-test'), __('Test Tools','menu-test'), 'manage_options', 'testtools', 'mt_tools_page');
+        }
+
+        // hydi_add_activity_page() displays the page content for the first submenu
+        // of the custom Test Toplevel menu
+        function hydi_add_activity_page() {
+            echo "<h2>" . __( 'Add an Activity', 'menu-test' ) . "</h2>";
+            include 'new-activity-form.php';
+        }
+
+         // mt_sublevel_page2() displays the page content for the second submenu
+        // of the custom Test Toplevel menu
+        function hydi_update_activity_page() {
+            echo "<h2>" . __( 'Update an Activity', 'menu-test' ) . "</h2>";
+        }
+
+        // mt_sublevel_page2() displays the page content for the second submenu
+        // of the custom Test Toplevel menu
+        function hydi_pending_activities_page() {
+            echo "<h2>" . __( 'Pending Activities', 'menu-test' ) . "</h2>";
+        }
+
+        /*
+        // mt_settings_page() displays the page content for the Test settings submenu
+        function mt_settings_page() {
+            echo "<h2>" . __( 'Test Settings', 'menu-test' ) . "</h2>";
+        }
+
+        // mt_tools_page() displays the page content for the Test Tools submenu
+        function mt_tools_page() {
+            echo "<h2>" . __( 'Test Tools', 'menu-test' ) . "</h2>";
+        }
+
+       
+        */
+    }
+?>
---
+++ hydi-admin/new-activity-form.php
@@ -0,0 +1,143 @@
+<?php
+/**
+* New activity form
+* Adds a new activity into database
+*/
+
+/*========================================
+Form Fields:
+(*) - mandatory
+- Post ID(*)
+- Category
+- Name(*)
+- Description
+- Address(*)
+- Region
+- Country
+- Phone Number
+- Website
+- Suitable for how many persons
+- Average price
+- Opening hours 
+========================================*/
+?>
+<form action="functions.php" method="POST">
+	<table>
+		<tr>
+			<td>Post ID *</td>
+			<td><input type="text" name="postid"/></td>
+		</tr>
+		<tr>
+			<td>Category</td>
+			<td>
+				<input type="checkbox" name="category" value="Eat" />Eat
+				<input type="checkbox" name="category" value="Play" />Play
+				<input type="checkbox" name="category" value="Explore" />Explore
+			</td>
+		</tr>
+		<tr>
+			<td>Name *</td>
+			<td><input type="text" name="name"></td>
+		</tr>
+		<tr>
+			<td>Description</td>
+			<td><textarea rows="4" cols="50" maxlength="50" name="description"></textarea></td>
+		</tr>
+		<tr>
+			<td>Address *</td>
+			<td><textarea rows="4" cols="50" maxlength="50" name="address"></textarea></td>
+		</tr>
+		<tr>
+			<td>Region</td>
+			<td>
+				<select>
+					<option value="North">North</option>
+					<option value="South">South</option>
+					<option value="East">East</option>
+					<option value="West">West</option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td>Country</td>
+			<td>
+				<select>
+					<option value="Singapore">Singapore</option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td>Phone number</td>
+			<td><input type="text" name="phone"></td>
+		</tr>
+		<tr>
+			<td>Website</td>
+			<td><input type="text" name="website"></td>
+		</tr>
+		<tr>
+		<td>Suitable for how many number of person</td>
+		<td>
+			<select>
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+				<option value="5">5</option>
+				<option value="6">6</option>
+				<option value="7">7</option>
+				<option value="8">8</option>
+				<option value="9">9</option>
+				<option value="10">10</option>
+			</select>
+			&nbsp;-&nbsp;
+			<select>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+				<option value="5">5</option>
+				<option value="6">6</option>
+				<option value="7">7</option>
+				<option value="8">8</option>
+				<option value="9">9</option>
+				<option value="10">10</option>
+			</select>
+		</td>
+		</tr>
+			<tr>
+				<td>Average price</td>
+				<td><input type="text" name="price"></td>
+			</tr>
+			<tr>
+				<td></td>
+			</tr>
+			<tr>
+				<td>Opening hours</td>
+				<td>
+					<input type="checkbox" id="chkBox_opHrs" value="24 Hours">24 Hours<br>
+					<input id="fromTime" type="text" class="time ui-timepicker-input" data-scroll-default="6:00am" autocomplete="off">
+					&nbsp;-&nbsp;
+					<input id="toTime" type="text" class="time ui-timepicker-input" data-scroll-default="6:00am" autocomplete="off">
+				</select>
+			</td>
+		</tr>
+	</table>
+	<br><br>
+	<input type="submit" value="Submit">
+</form>
+<script>
+	jQuery( document ).ready(function() {
+		jQuery('#chkBox_opHrs').change(function(){
+			if (jQuery('#chkBox_opHrs').is(':checked')) {
+				document.getElementById("fromTime").disabled=true;
+				document.getElementById("toTime").disabled=true;
+			}
+			else{
+				document.getElementById("fromTime").disabled=false;
+				document.getElementById("toTime").disabled=false;
+			}
+		});
+		jQuery('#fromTime').timepicker();
+		jQuery('#toTime').timepicker();
+	});
+	
+</script>
